123c55d67d7eef92bab5d3a98db17a3000680d35
Giovanna Zanelato <giovanna.zanelato03@gmail.com>
2025-08-14 00:49:20 +0000

    1

---
+++ Aula 1 /aula1_teoria.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct{
+    int values[LEN];
+    int n;
+}list;
+
+int is_full(list *l){
+   return l->n == LEN     quando usa return antes de uma omoa     
+} 
+
+int main(){
+    printf("heloo world");
+    return 0;
+}
