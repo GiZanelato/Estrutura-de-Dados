75b39abab7c3fd945c249e2d84b45c2143729e27
Giovanna Zanelato <giovanna.zanelato03@gmail.com>
2025-09-30 21:33:21 +0000

    feito

---
+++ Aula6_lab/Aula6.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Celula {
+	int valor;
+	struct Celula *proximo;
+	struct Celula *anterior;
+} Celula;
+
+typedef struct {
+	Celula *head;
+	Celula *tail;
+	int qtde;
+} Queue;
+
+// Celula *cria_celula(int valor){
+// 	Celula *celula = malloc(sizeof(Celula));
+// 	celula->proximo = NULL;
+// 	celula->anterior = NULL;
+// 	celula->valor = valor;
+
+// 	return celula;
+// }
+
+Queue *cria_queue(){
+	Queue *queue = malloc(sizeof(Queue));
+	queue->head = NULL;
+	queue->tail = NULL;
+	queue->qtde = 0;
+
+	return queue;
+}
+
+void enqueue(Queue *queue, int valor){
+	Celula *nova = malloc(sizeof(Celula));
+    nova->proximo = NULL;
+    nova->anterior = NULL;
+    nova->valor = valor;
+
+    if(queue->qtde == 0){
+        queue->head = nova;
+        queue->tail = nova;
+    }
+    else{
+        queue->tail->proximo = nova;
+        nova->anterior = queue->tail;
+        queue->tail = nova;
+    }
+
+    queue->qtde++;
+}
+
+int dequeue(Queue *queue){
+    int valor = -1;
+    if(queue->qtde == 0) return valor;
+
+    if(queue->qtde == 1){
+        valor = queue->head->valor;
+        free(queue->head);
+        queue->head = NULL;
+        queue->tail = NULL;
+    }
+    else{
+        valor = queue->head->valor;
+        queue->head = queue->head->proximo;
+        free(queue->head->anterior);
+        queue->head->anterior = NULL;
+    }
+    queue->qtde--;
+    return valor;
+}
+
+void show(Queue *queue){
+    Celula *atual = queue->head;
+    while(atual != NULL){
+        printf("%d", atual->valor);
+        atual = atual->proximo;
+    }
+    printf("\n");
+}
+
+void show_invertido(Queue *queue){
+	Celula *atual = queue->tail;
+    while(atual != NULL){
+        printf("%d", atual->valor);
+        atual = atual->anterior;
+    }
+}
+
+int main(void) {
+	Queue *qnum1 = cria_queue();
+	Queue *qnum2 = cria_queue();
+	Queue *res = cria_queue();
+	
+	char num1[100];
+	char num2[100];
+	fgets(num1, sizeof(num1), stdin);
+	fgets(num2, sizeof(num2), stdin);
+
+	int i = 0;
+	while(num1[i] != '\n'){		
+		enqueue(qnum1, (int)(num1[i]) - 48);
+		enqueue(qnum2, (int)(num2[i]) - 48);
+		i++;
+	}
+
+    Celula *atual1 = qnum1->tail;
+    Celula *atual2 = qnum2->tail;
+    int soma;
+    int valor;
+    int estouro = 0;
+
+   while(atual1 != NULL){
+        soma = atual1->valor + atual2->valor + estouro;
+        if(soma >= 10){
+            valor = soma - 10;
+            enqueue(res,valor);
+            estouro = 1;
+        }
+        else{
+            enqueue(res,soma);
+            estouro = 0;
+        }
+        atual1 = atual1->anterior;
+        atual2 = atual2->anterior;
+   }
+
+   if(estouro == 1){
+    enqueue(res,estouro);
+   }
+
+    show_invertido(res);
+ 
+
+} 
